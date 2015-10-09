2748091ab60a0073ecd678bd81e10c425495926b
George Mathew <panzer@Georges-MacBook-Pro-2.local>
2015-10-09 17:36:50 -0400

    Parallelized GALE rudimentary #6

--- Code/runner.py
+++ Code/runner.py
@@ -1,12 +1,12 @@
 from __future__ import print_function, division
 from problems.dtlz2 import DTLZ2
-from algorithms.serial.gale.gale import GALE
+from algorithms.parallel.gale.gale import GALE, run
 
 if __name__ == "__main__":
   model = DTLZ2()
   gale = GALE(model)
-  goods = gale.run()
+  run(gale)
   #print(gale.convergence(goods))
   #print(gale.diversity(goods))
-  gale.solution_range(goods)
+  #gale.solution_range(goods)
 
