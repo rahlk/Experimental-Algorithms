d4913e3f5bb6f0e5ae6940ce5d8a214b6c9176d3
Rahul Krishna <i.m.ralk@gmail.com>
2015-10-15 16:26:21 -0400

    commit with a makefile

--- MPItest/sampleMPI.py
+++ MPItest/sampleMPI.py
@@ -5,7 +5,7 @@ comm=MPI.COMM_WORLD
 rank=comm.rank
 size=comm.size
 
-for h in xrange(400):
+for j in xrange(400):
   data = j**2
   comm.send(data, dest=(rank+1)%size)
   data1=comm.recv(source=(rank-1)%size)
