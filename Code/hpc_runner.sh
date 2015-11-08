b8adb183f95f5006ffc63dd66664a907b28f7def
George Mathew <george.meg91@gmail.com>
2015-11-07 21:08:17 -0500

    Hpc runner

---
+++ Code/hpc_runner.sh
@@ -0,0 +1,8 @@
+#! /bin/tcsh
+
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number parameters"
+  exit 1
+fi
+
+bsub -W 6000 -n $1 -o out/$2_$1.out.%J -e err/$2_$1.err.%J sh runner.sh $1 $2 > log/$2_$1.log.%J
